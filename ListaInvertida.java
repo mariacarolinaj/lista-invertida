1b3522740ea8388caabd622c811a750a0bc9c5fc
Maria Carolina <mariacarolina_bh@hotmail.com>
2020-06-03 13:02:12 -0300

    Inclui cabeçalho

--- ListaInvertida.java
+++ ListaInvertida.java
@@ -1,3 +1,10 @@
+/*
+ * Lista Invertida
+ * Trabalho prático para a disciplina de Algoritmos e Estruturas de Dados III do curso de Ciência da Computação
+ * Implementado por Maria Carolina Resende Jaudacy em 1/2020 • Matrícula 667477
+ * PUC Minas - Professor Marcos Kutova
+ */
+
 public class ListaInvertida {
 
 }
